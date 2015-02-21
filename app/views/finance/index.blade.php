12adead0a20c0b09a9e360ecd5e643679d269bf3
Mix Skyline <mixman_99@hotmail.com>
2015-02-22 02:30:33 +0700

    commit

--- app/views/finance/index.blade.php
+++ app/views/finance/index.blade.php
@@ -8,8 +8,18 @@
 @stop
 @section('content')
 <div>
-	<form name="form-sep">
+	<form name="form-sep">			
 		<div class="text-right" style="padding-bottom:1%">
+			<!-- Search Box -->
+			<div class="input-group input-search">
+				<input type="text" id="txt_date" class="date-picker form-control" placeholder="ค้นหา : วันที่ลงบัญชี">
+	            <input type="text" id="txt_keyword" class="form-control" placeholder="ค้นหา : รายละเอียด">
+	            <span class="input-group-btn">
+	                <button class="btn btn-default btn-primary" id='btn_search' type="button">
+	                    <span class="icon-search"></span>
+	                </button>
+	            </span>
+	        </div>
 			<a class="btn btn-primary" href="{{ url('form-finance') }}">
 				<i class="icon-plus-sign icon-white"></i>&nbsp;
 				<span lang="En" >เพิ่ม</span>
@@ -20,5 +30,59 @@
 		</div>	
 	</form>		
 </div>	
+<script type="text/javascript">
+$(document).ready(function(){
+	$(".date-picker").datepicker({
+		format: 'dd-mm-yyyy',
+	});
+	$(".date-picker").on("change", function () {
+	    var id = $(this).attr("id");
+	    var val = $("label[for='" + id + "']").text();
+	    $("#msg").text(val + " changed");
+	});
+	// Search //
+	var perpage = {{ $arr_perpage['finance'] }};
 
+	$("#btn_search").click(function(){
+            var keyword = $("#txt_keyword").val();
+            var keydate = $("#txt_date").val();
+            Search(1,keyword,keydate);
+    });
+
+    $("ul.pagination.finance li a").click(function(){
+            var arr_id = (this.id).split("_");
+            var page = arr_id.pop();
+            var keyword = {{ $keyword }}
+
+            SearchShop(page,keyword);
+
+            return false;
+        });
+
+	$("#txt_keyword").keypress(function(e){  
+            if (e.keyCode == 13) {
+                var keyword = $("#txt_keyword").val();
+                Search(1,keyword,null);
+            }
+	});
+
+	$("#txt_date").keypress(function(e){  
+            if (e.keyCode == 13) {
+                var keydate = $("#txt_date").val();
+                Search(1,null,keydate);
+            }
+	});
+
+	function Search(page,keyword,keydate){
+            $.ajax({
+                type:"POST",
+                url:"{{ url('search-finance') }}",
+                data:{ page: page, perpage: perpage, keyword: keyword ,keydate:keydate },
+                success:function(result){
+                    $("div#tbl").html(result);
+                }
+            });
+	}
+});
+</script>
 @stop
